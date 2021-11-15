7132a7a85af9459fa7e0146e32b2e1510cd923de
rishi <rishi154@gmail.com>
2021-11-15 14:44:25 +0530

    made changes for Java11

--- api-gateway/src/main/java/com/nvrs/gateway/exception/JwtTokenMissingException.java
+++ api-gateway/src/main/java/com/nvrs/gateway/exception/JwtTokenMissingException.java
@@ -1,11 +1,9 @@
 package com.nvrs.gateway.exception;
 
 import javax.naming.AuthenticationException;
-import java.io.Serial;
 
 public class JwtTokenMissingException extends AuthenticationException {
 
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public JwtTokenMissingException(String msg) {
